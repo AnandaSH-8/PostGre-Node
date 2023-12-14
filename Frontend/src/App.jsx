def37b995b78a0cee3ff6cbc28752e3b66364a84
Anands-88 <anandsholla8@gmail.com>
2023-12-15 00:25:02 +0530

    todo file setups

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import CreateTodo from './Components/todoComp'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <CreateTodo></CreateTodo>
     </>
   )
 }
---
+++ Frontend/src/Components/todoComp.jsx
@@ -0,0 +1,10 @@
+
+export default function CreateTodo(){
+
+    return (
+        <>
+        <h1>Create Todo</h1>
+        </>
+    )
+}
+
